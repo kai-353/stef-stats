98fb3b770f706a5388605950259ea9ce0dc9cd50
kai-353 <70913300+kai-353@users.noreply.github.com>
2022-02-16 22:00:39 +0100

    Added the beginnings of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyChart from "./components/MyChart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [trophyChartData, setTrophyChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Trophies",
+        data: [],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  });
+
+  const [trophyChartOptions, setTrophyChartOptions] = useState({
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "Amount of trophies",
+      },
+    },
+  });
+
+  useEffect(() => {
+    fetch("https://gmc.io-jpt.nl/api/days_in_arena.php")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+
+    fetch("https://gmc.io-jpt.nl/api/min_trophies.php")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+
+    fetch("https://gmc.io-jpt.nl/api/max_trophies.php")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+
+    fetch("https://gmc.io-jpt.nl/api/get_best_of_each_day.php")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setTrophyChartData({
+          labels: result.map((x) => x["created_at"]),
+          datasets: [
+            {
+              label: "Trophies",
+              data: result.map((x) => x["trophies"]),
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ],
+        });
+      });
+
+    fetch("https://gmc.io-jpt.nl/api/get_arenas.php")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyChart data={trophyChartData} options={trophyChartOptions} />
     </div>
   );
 }
